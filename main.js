54a589dcf399afd18d904742ba8eb64f9713a1c8
Amit Shah <amitshah0t7@gmail.com>
2020-05-20 05:57:57 -0400

    added array support and point encoding

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 * @Author: amitshah
 * @Date:   2020-05-17 05:11:37
 * @Last Modified by:   amitshah
-* @Last Modified time: 2020-05-17 19:15:39
+* @Last Modified time: 2020-05-20 05:55:38
 */
 
 
@@ -11,7 +11,7 @@ var bn = require('bn.js');
 const curve = new ec("p256");
 const hash = (msg)=>{ return curve.hash().update(msg).digest("hex"); }; 
 const red = bn.red(curve.curve.p);
-
+var crypto = require('crypto');
 
 
 function readBit(buffer, i, bit){
@@ -56,7 +56,6 @@ function tryPoint(r){
 }
 
 
-
 class Alice{
 	constructor(){
 		this.keyPair = curve.genKeyPair();
@@ -84,7 +83,7 @@ class Alice{
 		var c1 = this.keyPair.getPublic();
 		//G^r1r2*x*H(alice)
 		var c2 = this.serverKey.mul(this.keyPair.getPrivate()).mul(hash(d))
-		return [c1,c2];
+		return [c1.encodeCompressed('hex'),c2.encodeCompressed('hex')];
 	}
 }
 
@@ -97,20 +96,62 @@ class Bob{
 		
 		var h = hash(msg)
 		//G^x*r2*H(bob)+y
+		console.log("PHASE_2",p1.mul(h).add(this.keyPair.getPublic()).encodeCompressed('hex'));
 		return p1.mul(h).add(this.keyPair.getPublic())
+	}
+
+	phase2_b(encodedKeys,msg){
+		var self = this;
+		return encodedKeys.map(ek=>{
+			var p1 = curve.curve.decodePoint(ek,'hex');
+			var h = hash(msg)
+			console.log("PHASE_2B:",p1.mul(h).add(this.keyPair.getPublic()).encodeCompressed('hex'))
+			return p1.mul(h).add(this.keyPair.getPublic());
+		}).map(p1=>{
+			return p1.encodeCompressed('hex');
+		});	
 	}
 
 	phase4(p3,pn,serverPk){
 		var actual = p3.add(pn.neg());
 		var expectedPk = serverPk.mul(this.keyPair.getPrivate());
+		console.log( actual.getX().toString(16), expectedPk.getX().toString(16) , 
+		actual.getY().toString(16) , expectedPk.getY().toString(16) )
 		return actual.getX().toString(16) === expectedPk.getX().toString(16) && 
 		actual.getY().toString(16) === expectedPk.getY().toString(16) 
+	}
+
+	phase4_b(p3s, pnEncoded, serverPkEncoded){
+		var self = this;
+		var serverPk = curve.curve.decodePoint(serverPkEncoded,'hex');
+		var expectedPkEncoded = serverPk.mul(this.keyPair.getPrivate()).encodeCompressed('hex'); 
+		var pn = pnEncoded.map(pn=>{
+			return curve.curve.decodePoint(pn,'hex');
+		})
+		var p3 = p3s.map(ek=>{
+			return curve.curve.decodePoint(ek,'hex');	
+		});
+
+		for(var i=0; i < p3.length; i++){
+
+			for (var j=0; j< pn.length; j++){
+				var actualPkEncoded = p3[i].add(pn[j].neg()).encodeCompressed('hex');
+				if(actualPkEncoded === expectedPkEncoded){
+					console.log("Found Match. i:",i,"j:",j);
+					break;
+				}else{
+					console.log("No Match. i:",i,"j:",j);
+				}
+			}
+
+		}
 	}
 }
 
 class Server{
 	constructor(){
 		this.keyPair = curve.genKeyPair();
+		this.dataSet = {};
 	}
 
 	getSharedPublicKey(publicKey){
@@ -125,21 +166,49 @@ class Server{
 		//G^(x*r2*H(bob)+y)*r1
 		return p2.mul(this.keyPair.getPrivate());
 	}
+
+	phase3_b(p2s){
+		var self = this;
+		return p2s.map(ek=>{
+			var p2 = curve.curve.decodePoint(ek,'hex');
+			return self.phase3(p2);
+		}).map(p2=>{
+			return p2.encodeCompressed('hex');
+		})
+	}
+
 	setAliceData(c1,c2){
 		//c1 = G^x
 		//c2 = //G^r1*r2*x*H(alice)
 		this.data = [c1,c2];
 	}
+
+	upload(infectedData){
+		for(var i=0; i< infectedData.length; i++){
+			var id = infectedData[i];
+			var key = curve.curve.decodePoint(id[0],'hex').mul(this.keyPair.getPrivate()).encodeCompressed('hex');
+			if(!this.dataSet.hasOwnProperty(key)){
+				this.dataSet[key] = [];
+			}
+			this.dataSet[key].push(id[1]);
+		}		
+	}
+
+
 }
 
 class Server2 extends Server{
 
-	
 
 	phase1(){
 		//G^x*r2
 		return [this.data[0].mul(this.keyPair.getPrivate()),this.data[1]];
 	}
+
+	phase1_b(){
+		return [Object.keys(this.dataSet),Object.values(this.dataSet).flat()];
+	}
+
 
 	phase4(p3){
 		var c2= this.data[1];
@@ -159,63 +228,93 @@ S2 = new Server2();
 
 
 //Offline Mode
-
 Alice = new Alice();
 Alice.compareAndSetServerKey([S2.getSharedPublicKey(S1.getPublic()),S1.getSharedPublicKey(S2.getPublic())]);
-let [c1,c2] = Alice.encodeData("geohash");
 
-S2.setAliceData(c1,c2);
-S1.setAliceData(c1,c2);
+var InfectedData = [];
+
+const INFECION_DATA_SIZE = 288*14;
+
+for(var i=0; i< INFECION_DATA_SIZE; i++){
+	
+	if(Math.random() > 0.999){
+		console.log("ADDED at position:",i)
+		let [c1,c2] = Alice.encodeData("geohash");
+		InfectedData.push([c1,c2]);
+	}else{
+		InfectedData.push(Alice.encodeData(crypto.randomBytes(32)));
+	}
+	console.log("generating infected data:",i,":",INFECION_DATA_SIZE);
+}
+
+
+
+S2.upload(InfectedData);
+//S1.upload(InfectedData);
 
 //Bob requests data from Server
 Bob = new Bob();
-let [p1,p4] = S2.phase1();
+// let [p1,p4] = S2.phase1();
 
-p2 = Bob.phase2(p1, "geohash2");
+// p2 = Bob.phase2(p1, "geohash2");
 
-p3 = S1.phase3(p2);
+// p3 = S1.phase3(p2);
 
-if(!Bob.phase4(p3,p4,S1.keyPair.getPublic())){
- console.log("invalid");
-}else{
-	 console.log("valid");
-}
-
-
-//G^yr1
-rActual = S2.phase4(p3)
-
-rExpected = S1.keyPair.getPublic().mul(Bob.keyPair.getPrivate())
-
-console.log(rActual.getX().toString(16));
-console.log(rExpected.getX().toString(16));
-console.log(rActual.getY().toString(16));
-console.log(rExpected.getY().toString(16));
+// if(!Bob.phase4(p3,p4,S1.keyPair.getPublic())){
+//  console.log("invalid");
+// }else{
+// 	 console.log("valid");
+// }
 
 
-p2 = Bob.phase2(p1, "geohash");
+// //G^yr1
+// rActual = S2.phase4(p3)
 
-p3 = S1.phase3(p2);
+// rExpected = S1.keyPair.getPublic().mul(Bob.keyPair.getPrivate())
 
-if(!Bob.phase4(p3,p4,S1.keyPair.getPublic())){
- console.log("invalid");
-}else{
-	 console.log("valid");
-}
-//G^yr1
-rActual = S2.phase4(p3)
-
-rExpected = S1.keyPair.getPublic().mul(Bob.keyPair.getPrivate())
-
-console.log(rActual.getX().toString(16));
-console.log(rExpected.getX().toString(16));
-console.log(rActual.getY().toString(16));
-console.log(rExpected.getY().toString(16));
+// console.log(rActual.getX().toString(16));
+// console.log(rExpected.getX().toString(16));
+// console.log(rActual.getY().toString(16));
+// console.log(rExpected.getY().toString(16));
 
 
+// p2 = Bob.phase2(p1, "geohash");
+
+// p3 = S1.phase3(p2);
+
+// if(!Bob.phase4(p3,p4,S1.keyPair.getPublic())){
+//  console.log("invalid");
+// }else{
+// 	 console.log("valid");
+// }
+// //G^yr1
+// rActual = S2.phase4(p3)
+
+// rExpected = S1.keyPair.getPublic().mul(Bob.keyPair.getPrivate())
+
+// console.log(rActual.getX().toString(16));
+// console.log(rExpected.getX().toString(16));
+// console.log(rActual.getY().toString(16));
+// console.log(rExpected.getY().toString(16));
+
+var start = Date.now();
+console.log("Processing Start:",start);
 
 
+let [encodedC1, encodedC2] = S2.phase1_b();
 
+encodedPhase2= Bob.phase2_b(encodedC1,"geohash");
+
+encodedPhase3 = S1.phase3_b(encodedPhase2);
+
+Bob.phase4_b(encodedPhase3, encodedC2,S1.keyPair.getPublic().encodeCompressed('hex'))
+
+console.log("Time Delta:",Date.now()-start);
+
+// console.log(c1.encodeCompressed('hex'));
+// console.log(c2.encodeCompressed('hex'));
+// console.log(p1.encodeCompressed('hex'));
+// console.log(p4.encodeCompressed('hex'));
 
 
 
